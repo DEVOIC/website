057efa2d47e951b6ee6e8a3e22db3f341d2633c9
Shivansh Gupta <95837362+gshivg@users.noreply.github.com>
2023-08-01 18:41:37 +0530

    Added Redirects

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,16 @@
 <?php
 
+use App\Http\Controllers\TeamRedirectController;
 use Illuminate\Support\Facades\Route;
 
 
 //create routes view for home about and other pages
-Route::view('/','pages/home')->name('home');
-Route::view('/about','pages/about')->name('about');
-Route::view('/internships','pages/internships')->name('internships');
-Route::view('/services','pages/services')->name('services');
+// Route::view('/','pages/home')->name('home');
+// Route::view('/about','pages/about')->name('about');
+// Route::view('/internships','pages/internships')->name('internships');
+// Route::view('/services','pages/services')->name('services');
+
+Route::get('/', [TeamRedirectController::class, "redirectWebsite"]);
+
+Route::get('/{id}', [TeamRedirectController::class, "redirectMembers"]);
+Route::get('/dept/{id}', [TeamRedirectController::class, "redirectDepartments"]);
