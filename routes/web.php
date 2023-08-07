1e1fc8784421b7c1ee26fa2da2d1afd8291c4667
sumitchoudharySEM <sumitsem2004@gmail.com>
2023-08-07 10:25:53 +0530

    SOME MINAR UPDATES IN APP FOLDER

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,10 @@
 <?php
 
-use App\Http\Controllers\TeamRedirectController;
 use Illuminate\Support\Facades\Route;
 
 
 //create routes view for home about and other pages
-// Route::view('/','pages/home')->name('home');
-// Route::view('/about','pages/about')->name('about');
-// Route::view('/internships','pages/internships')->name('internships');
-// Route::view('/services','pages/services')->name('services');
-
-Route::get('/', [TeamRedirectController::class, "redirectWebsite"]);
-
-Route::get('/{id}', [TeamRedirectController::class, "redirectMembers"]);
-Route::get('/dept/{id}', [TeamRedirectController::class, "redirectDepartments"]);
+Route::view('/','pages/home')->name('home');
+Route::view('/about','pages/about')->name('about');
+Route::view('/internships','pages/internships')->name('internships');
+Route::view('/services','pages/services')->name('services');
